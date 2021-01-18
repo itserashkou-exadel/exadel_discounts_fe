6a69fd9d6b25c164700b40ccf0d3edd657f0b0ed
Denis Melnik <58951438+frntprog@users.noreply.github.com>
2021-01-18 16:38:19 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+   <Sign/>
+    <router-view/>
+    </div>
+</template>
+
+<script>
+    // import Sign from '@/components/Sign'
+    export default {
+        data() {
+            return {
+                showPassword: false
+            }
+        },
+        components: {
+            // Sign
+        }
+    }
+</script>
+
+<style>
+
+</style>
