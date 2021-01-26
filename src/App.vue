9032e17280534daca382b6b65962cd483f69b65e
ExiHo <exihomoty@gmail.com>
2021-01-26 16:54:05 +0300

    create cards

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,9 @@
 <template>
     <v-app>
-        <!--      <v-container>-->
-        <!--          <v-row justify="start" >-->
-        <!--              <v-col cols="4">-->
-        <!--                  <Typography />-->
-        <!--              </v-col>-->
-        <!--              <v-col cols="4">-->
-        <!--                  <Typography />-->
-        <!--              </v-col>-->
-        <!--              <v-col cols="4" >-->
-        <!--                  <Typography />-->
-        <!--              </v-col>-->
-        <!--              <v-col cols="4" >-->
-        <!--                  <Typography />-->
-        <!--              </v-col>-->
-        <!--              <v-col cols="4" >-->
-        <!--                  <Typography />-->
-        <!--              </v-col>-->
-        <!--          </v-row>-->
-        <!--          <v-spacer class="my-16"></v-spacer>-->
-        <!--          <v-row justify="space-between">-->
-        <!--              <v-col col="4">-->
-        <!--                  <Typography />-->
-        <!--              </v-col>-->
-        <!--              <v-col col="4">-->
-        <!--                  <Typography />-->
-        <!--              </v-col>-->
-        <!--              <v-col col="4">-->
-        <!--                  <Typography />-->
-        <!--              </v-col>-->
-        <!--          </v-row>-->
-
         <Cards/>
-<!--        :descriptions2="descriptions2"-->
-
-
-
-
-
         <v-main>
-
-            <!-- Provides the application the proper gutter -->
-
-
-            <!-- If using vue-router -->
             <router-view></router-view>
-
         </v-main>
-
     </v-app>
 </template>
 
