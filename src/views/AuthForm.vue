926f1f8079939f898651e35fd1155665df095e86
stigmaby@gmail.com <oFFline13>
2021-01-26 16:26:48 +0300

    CP-1-Implemented new login page menu. Created common menu for choosing country and town in one form area.

--- src/views/AuthForm.vue
+++ src/views/AuthForm.vue
@@ -1,32 +1,68 @@
 <template>
-    <div class="wrapper">
-        <v-card width="33%" class="mx-auto mt-15" color="blue-grey lighten-4" hover >
-            <v-card-title>Welcome to CrazyPrice App</v-card-title>
-            <v-card-text>
-                <v-form>
-                    <v-autocomplete label="Choose your country" :items="location.countries"></v-autocomplete>
-                    <v-autocomplete label="Choose your country" :items="location.towns"></v-autocomplete>
-                    <v-card-actions class="d-flex justify-center" >
-                        <v-btn color="success" width="150">Login</v-btn>
-                    </v-card-actions>
-                </v-form>
+
+    <v-container class="d-flex align-center wrapper" >
+        <v-card width="500" class="mx-auto ">
+            <v-card-text >
+
+                <v-list-group eager>
+                    <template v-slot:activator>
+                        <v-list-item-title>Choose your location</v-list-item-title>
+                    </template>
+
+                    <!--            Первая группа            -->
+                    <v-list-group no-action sub-group>
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>Belarus</v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+
+                        <v-list-item-group v-model="fictionalSelected">
+                            <v-list-item v-bind:key="town" v-for="town in belarus" @click="redirectToIS4">
+                                <v-list-item-title>{{ town }}</v-list-item-title>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list-group>
+                    <v-divider></v-divider>
+                    <!--            Вторая группа            -->
+                    <v-list-group sub-group no-action>
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>Ukraine</v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+                        <v-list-item-group v-model="realSelected">
+                            <v-list-item v-bind:key="town" v-for="town in ukraine ">
+                                <v-list-item-title >{{ town }}</v-list-item-title>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list-group>
+                </v-list-group>
+                <v-container class="d-flex justify-center">
+                    <v-btn >Login</v-btn>
+                </v-container>
+
             </v-card-text>
         </v-card>
-    </div>
+    </v-container>
 </template>
 
 <script>
     export default {
-        name: "AuthForm",
-        data() {
-            return {
-                location: {
-                    countries: ['Belarus', 'Ukraine', 'USA',],
-                    towns: ['Grodno', 'Minsk', 'Brest',],
-                },
+        name: 'Test2',
+        data: () => ({
+            belarus: ['Grodno', 'Minsk', 'Brest'],
+            fictionalSelected: null,
+            ukraine: ['Kiew'],
+            realSelected: null,
+        }),
+        methods: {
+            redirectToIS4(){
+                console.log('You was redirected')
+                this.$router.push('/identity')
             }
-        }
-    }
+        },
+    };
 </script>
 
 <style scoped>
---
+++ src/views/AuthForm_Old_Version.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-container  class="d-flex align-center wrapper">
+        <v-card width="33%" class="mx-auto mt-15 " color="blue-grey lighten-4" hover >
+            <v-card-title>Welcome to CrazyPrice App</v-card-title>
+            <v-card-text>
+                <v-form>
+                    <v-autocomplete label="Choose your country" :items="location.countries"></v-autocomplete>
+                    <v-autocomplete label="Choose your country" :items="location.towns"></v-autocomplete>
+                    <v-card-actions class="d-flex justify-center" >
+                        <v-btn color="success" width="150">Login</v-btn>
+                    </v-card-actions>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "AuthForm",
+        data() {
+            return {
+                location: {
+                    countries: ['Belarus --------', 'Grodno', 'Minsk',  'Ukraine', 'USA',],
+                    towns: ['Grodno', 'Minsk', 'Brest',],
+                },
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .wrapper {
+        height: 100vh;
+    }
+</style>
