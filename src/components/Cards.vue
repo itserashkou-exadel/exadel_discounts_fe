a24dbe1f8aba90ad2ddf0adc3db6d51e1d4297ea
ExiHo <exihomoty@gmail.com>
2021-01-27 17:29:43 +0300

    added cards to develop

---
+++ src/components/Cards.vue
@@ -0,0 +1,113 @@
+<template>
+    <v-container>
+        <v-row class="mt-12">
+            <v-col lg="4"
+                   sm="12"
+                   md="6"
+                   v-for="description in descriptions"
+                   :key="description.id">
+                <Card
+                        :description="description"
+                        />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script>
+    import Card from '../components/Card.vue'
+
+    export default {
+        name: "Cards",
+        data() {
+            return {
+                descriptions: [
+                    {
+                        id: 1,
+                        name: "Burger",
+                        discount: 50,
+                        rating: 4,
+                        info: " lorem ipsum balbalba orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalbaorem ipsum balbalba...",
+                        tags: [
+                            "tag", "tag", "tag"
+                        ]
+                    },
+                    {
+                        id: 2,
+                        name: "Burger",
+                        discount: 50,
+                        rating: 4.5,
+                        info: " lorem ipsum balbalba orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalbaorem ipsum balbalba...",
+                        tags: [
+                            "tag", "tag", "tag"
+                        ]
+                    },
+                    {
+                        id: 3,
+                        name: "Burger",
+                        discount: 50,
+                        rating: 4.5,
+                        info: " lorem ipsum balbalba orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalbaorem ipsum balbalba...",
+                        tags: [
+                            "tag", "tag", "tag"
+                        ]
+                    },
+                    {
+                        id: 4,
+                        name: "Burger",
+                        discount: 50,
+                        rating: 4.6,
+                        info: " lorem ipsum balbalba orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalbaorem ipsum balbalba...",
+                        tags: [
+                            "tag", "tag", "tag"
+                        ]
+                    },
+                    {
+                        id: 5,
+                        name: "Burger",
+                        discount: 50,
+                        rating: 4.5,
+                        info: " lorem ipsum balbalba orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalbaorem ipsum balbalba...",
+                        tags: [
+                            "tag", "tag", "tag"
+                        ]
+                    },
+                    {
+                        id: 6,
+                        name: "Burger",
+                        discount: 50,
+                        rating: 4.5,
+                        info: " lorem ipsum balbalba orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalba\n" +
+                            "            orem ipsum balbalbaorem ipsum balbalba...",
+                        tags: [
+                            "tag", "tag", "tag"
+                        ]
+
+                    },
+                ],
+
+            }
+        },
+        components: {
+            Card
+        }
+    }
+</script>
+<style scoped>
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,126 +20,7 @@
                 </v-col>
             </v-row>
         </v-app-bar>
-        <v-content>
-            <v-row>
-                <v-col cols="12" md="3" class="sidebar">
-                    <v-card
-                            :loading="loading"
-                            class="mx-auto my-12"
-                            max-width="374"
-                    >
-                        <v-img
-                                height="200"
-                                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                        ></v-img>
-                        <v-card-title>Croissant <span>-20%</span></v-card-title>
-                        <v-card-text>
-                            <v-row
-                                    align="center"
-                                    class="mx-0"
-                            >
-                                <v-rating
-                                        :value="4.5"
-                                        color="amber"
-                                        dense
-                                        half-increments
-                                        readonly
-                                        size="14"
-                                ></v-rating>
-
-                                <div class="grey--text ml-4">
-                                    4.5 (413)
-                                </div>
-                            </v-row>
-
-                            <div class="my-4 subtitle-1">
-                                $ • Lviv, Cafe
-                            </div>
-
-                            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus
-                                patio
-                                seating.
-                            </div>
-                        </v-card-text>
-
-                        <v-divider class="mx-4"></v-divider>
-
-                        <v-card-title>Unill 21.01 - 19:00</v-card-title>
-
-                        <v-card-actions>
-                            <v-btn
-                                    color="deep-purple lighten-2"
-                                    text
-                                    @click="reserve"
-                            >
-                                <h3>Apply</h3>
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                    <v-card
-                            :loading="loading"
-                            class="mx-auto"
-                            max-width="374"
-                    >
-
-
-                        <v-img
-                                height="250"
-                                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                        ></v-img>
-
-                        <v-card-title>Cafe Croissant</v-card-title>
-
-                        <v-card-text>
-                            <v-row
-                                    align="center"
-                                    class="mx-0"
-                            >
-                                <v-rating
-                                        :value="4.5"
-                                        color="amber"
-                                        dense
-                                        half-increments
-                                        readonly
-                                        size="14"
-                                ></v-rating>
-
-                                <div class="grey--text ml-4">
-                                    4.5 (413)
-                                </div>
-                            </v-row>
-
-                            <div class="my-4 subtitle-1">
-                                $ • Lviv, Cafe
-                            </div>
-
-                            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus
-                                patio
-                                seating.
-                            </div>
-                        </v-card-text>
-
-                        <v-divider class="mx-4"></v-divider>
-
-                        <v-card-title>Untill 21.01. 19:00</v-card-title>
-
-
-                        <v-card-actions>
-                            <v-btn
-                                    color="deep-purple lighten-2"
-                                    text
-                                    @click="reserve"
-                            >
-                                <h3>Apply</h3>
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-
-                </v-col>
-            </v-row>
-
-
-        </v-content>
+        <Cards/>
         <Footer/>
     </v-app>
 
@@ -147,10 +28,10 @@
 
 <script>
     import Footer from '@/components/Footer'
-
+    import Cards from '@/components/Cards'
     export default {
         name: "Home",
-        components: {Footer},
+        components: {Footer,Cards},
         data: () => ({
             loading: false,
             selection: 1,
