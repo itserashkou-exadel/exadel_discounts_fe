f88a5a01cee9ec69be9935bd2eafdb654f31bc21
ExiHo <exihomoty@gmail.com>
2021-01-27 11:59:40 +0300

    added icons to cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,59 +1,77 @@
 <template>
 
-                <v-card
-                        class="mx-4 my-12"
-                        max-width="374"
-                        height="auto"
+    <v-card
+            class="mx-4 my-12"
+            max-width="374"
+            height="570"
+    >
+        <div>
+            <v-img
+                    height="250"
+                    src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                    class="white--text "
+            >
+                <div>
+                    <v-avatar size="59" color="red" class=" mx-3 my-3"><b>{{description.discount}}%</b>
+                    </v-avatar>
+                </div>
+            </v-img>
+        </div>
+        <v-row align="center" justify="space-between">
+            <v-col>
+                <div>
+                    <v-card-title>
+                        {{ description.name }}
+                    </v-card-title>
+                </div>
+                <v-row
+                        align="center"
+                        class="ml-3 mt-n4 mb-3"
                 >
-                    <div>
-                        <v-img
-                                height="250"
-                                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                                class="white--text "
-                        >
-                            <div>
-                                <v-avatar size="59" color="red" class=" mx-3 my-3"><b>{{description.discount}}%</b>
-                                </v-avatar>
-                            </div>
-                        </v-img>
+                    <v-rating
+                            :value="description.rating"
+                            color="amber"
+                            dense
+                            half-increments
+                            readonly
+                            size="14"
+                    ></v-rating>
+                    <div class="grey--text ml-2">
+                        {{description.rating}}
                     </div>
-                    <div>
-                        <v-card-title>
-                            {{ description.name }}
-                        </v-card-title>
-                    </div>
-                    <v-row
-                            align="center"
-                            class="ml-3 mt-n4 mb-3"
-                    >
-                        <v-rating
-                                :value="description.rating"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="14"
-                        ></v-rating>
-                        <div class="grey--text ml-2">
-                            {{description.rating}}
-                        </div>
-                    </v-row>
-                    <v-divider></v-divider>
-                    <v-card-subtitle>
-                        <b>Description:<br></b>
-                        {{description.info}}
-                    </v-card-subtitle>
-                    <v-chip-group active-class="light-blue accent-4 white--text">
-                        <v-chip v-for="tag in description.tags"
-                                :key="tag"
-                                class="ml-4"> tag
-                        </v-chip>
-                    </v-chip-group>
-                    <v-btn class="ma-4" color="blue" raised>
-                        Choose
-                    </v-btn>
-                </v-card>
-
+                </v-row>
+            </v-col>
+            <v-icon
+                    v-on:click="colorSwitch"
+                    class="mr-13" large>
+                {{ card }}
+            </v-icon>
+        </v-row>
+        <v-divider></v-divider>
+        <v-card-subtitle>
+            <b>Description:<br></b>
+            {{description.info}}
+        </v-card-subtitle>
+        <v-chip-group active-class="light-blue accent-4 white--text">
+            <v-chip v-for="tag in description.tags"
+                    :key="tag"
+                    class="ml-4"> tag
+            </v-chip>
+        </v-chip-group>
+        <v-row class="mt-4 ml-4" align="center" justify="space-between">
+            <v-btn color="blue" raised>
+                Choose
+            </v-btn>
+            <div class="mr-12">
+                <v-icon @click="">
+                    mdi-pencil
+                </v-icon>
+                <v-icon class="mr-2 ml-2" @click="">
+                    mdi-delete
+                </v-icon>
+            </div>
+        </v-row>
+    </v-card>
 </template>
 <script>
     export default {
@@ -65,7 +83,17 @@
             }
         },
         data() {
-            return {}
+            return {
+                card:"mdi-heart-outline"
+            }
+        },
+        methods: {
+            colorSwitch() {
+                if(this.card==="mdi-heart-outline")
+                    this.card="mdi-heart"
+                else
+                    this.card="mdi-heart-outline"
+            }
         }
     }
 </script>
