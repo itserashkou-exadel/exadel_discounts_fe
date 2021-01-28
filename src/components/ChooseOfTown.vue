de2ce0d4e2e952fc2a58bc38626256f8b4b37c0f
Taya69 <27taya@mail.ru>
2021-01-28 17:58:28 +0300

    cp-4: Create page 'Adding discount' and component "ChooseTown"

---
+++ src/components/ChooseOfTown.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <v-list
+                ref="list"
+
+        >
+            <v-list-group>
+                <template v-slot:activator="{on, attrs}"
+                >
+                    <v-list-item-title
+                            v-model="listmodel"
+                            v-on="on"
+                            v-bind="attrs"
+                    >Choose your location</v-list-item-title>
+                </template>
+
+                <!--            Первая группа            -->
+                <v-list-group no-action sub-group>
+                    <template v-slot:activator>
+                        <v-list-item-content>
+                            <v-list-item-title
+
+                            >Belarus
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </template>
+
+                    <v-list-item-group v-model="fictionalSelected">
+                        <v-list-item v-for="town in belarus" :key="town.town">
+                            <v-list-item-title
+                                    @click="select(town)"
+                            >{{ town.town }}
+                            </v-list-item-title>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list-group>
+                <!--            Вторая группа            -->
+                <v-list-group sub-group no-action>
+                    <template v-slot:activator>
+                        <v-list-item-content>
+                            <v-list-item-title>Ukraine</v-list-item-title>
+                        </v-list-item-content>
+                    </template>
+                    <v-list-item-group v-model="fictionalSelected">
+                        <v-list-item v-for="town in ukraine" :key="town.id"
+                        >
+                            <v-list-item-title>{{ town.town }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list-group>
+            </v-list-group>
+        </v-list>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ChooseOfTown",
+        data() {
+            return {
+
+                fictionalSelected: null,
+                realSelected: null
+            }
+
+        },
+        props: ['belarus', 'ukraine', 'listmodel'],
+        methods: {
+            select(town) {
+                console.log(town);
+                this.$emit('select', town)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import AddingDiscount from './views/AddingDiscount.vue'
+
+
 
 Vue.use(Router);
 
@@ -12,12 +15,17 @@ export default new Router({
       component: Home,
     },
     {
+      path: '/AddingDiscount',
+      component: AddingDiscount
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
+    }
+
   ],
 });
