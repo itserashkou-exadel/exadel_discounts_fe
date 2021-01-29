0084e9aff747a672d81f21a8948d5ac63e4f197d
Natalie Smirnova <natalie.smirnowa@gmail.com>
2021-01-29 16:40:11 +0300

    cp-2: routing_for_Table_added

--- src/router.ts
+++ src/router.ts
@@ -7,82 +7,87 @@ import SubscribesPage from "@/views/SubscriptionsPage.vue";
 import MapPage from "@/views/MapPage.vue";
 import FavoritesPage from "@/views/FavoritesPage.vue";
 import AppTemplate from "@/views/AppTemplate.vue";
-import Table from '@/components/Table.vue'
+import Table from '@/components/Table.vue';
+import AddingDiscount from '@/views/AddingDiscount.vue';
 
 Vue.use(VueRouter);
 
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'sign',
-      component: Sign
-    },
-    {
-      path: '/table',
-      name: 'table',
-      component: Table
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: AppTemplate,
-      children: [
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
         {
-          path: '/home',
-          name: 'home',
-          component: HomePage,
-        }
-      ]
-    },
-    {
-      path: '/cards',
-      name: 'cards',
-      component: Cards
-    },
-    {
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      path: '/subscriptions',
-      name: 'subscriptions',
-      component: AppTemplate,
-      children: [
+            path: '/',
+            name: 'sign',
+            component: Sign
+        },
         {
-          path: "/subscriptions",
-          name: "subscriptions",
-          component: SubscribesPage
-        }
-      ]
-    },
-    {
-      path: '/map',
-      name: 'map',
-      component: AppTemplate,
-      children: [
+            path: '/',
+            name: 'table',
+            component: Table
+        },
         {
-          path: "/map",
-          name: "map",
-          component: MapPage
-        }
-      ]
-    },
-    {
-      path: '/favorites',
-      name: 'favorites',
-      component: AppTemplate,
-      children: [
+            path: '/home',
+            name: 'home',
+            component: AppTemplate,
+            children: [
+                {
+                    path: '/home',
+                    name: 'home',
+                    component: HomePage,
+                }
+            ]
+        },
         {
-          path: "/favorites",
-          name: "favorites",
-          component: FavoritesPage
+            path: '/home',
+            name: 'home',
+            component: AppTemplate,
+            children: [
+                {
+                    path: '/home/add_discount',
+                    name: 'add_discount',
+                    component: AddingDiscount,
+                }]
+        },
+        {
+
+            path: '/subscriptions',
+            name: 'subscriptions',
+            component: AppTemplate,
+            children: [
+                {
+                    path: "/subscriptions",
+                    name: "subscriptions",
+                    component: SubscribesPage
+                }
+            ]
+        },
+        {
+            path: '/map',
+            name: 'map',
+            component: AppTemplate,
+            children: [
+                {
+                    path: "/map",
+                    name: "map",
+                    component: MapPage
+                }
+            ]
+        },
+        {
+            path: '/favorites',
+            name: 'favorites',
+            component: AppTemplate,
+            children: [
+                {
+                    path: "/favorites",
+                    name: "favorites",
+                    component: FavoritesPage
+                }
+            ]
         }
-      ]
-    }
-  ]
+    ]
 });
 
 export default router;
