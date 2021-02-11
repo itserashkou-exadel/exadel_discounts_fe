a060bfab125f53a7547813847cb5a2a0fecba848
stigmaby@gmail.com <oFFline13>
2021-02-11 16:33:14 +0300

    Cp-003-Map-Fetch multiple markers: Added side bar menu

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
+    <div class="mapWrapper">
+        <div class="mapNavigation" >
+            <div class="objectWrapper" v-for="point in fakeDataPoints">
+                <h2>{{point.name}}</h2>
+                <p>{{point.description}}</p>
+                <p>{{point.address}}</p>
+                <button class="primary mapAddButton" >Добавить</button>
+            </div>
+        </div>
         <div id="mapContainer" class="basemap"></div>
     </div>
 
@@ -22,32 +30,37 @@
                 fakeDataPoints: [
                     {
                         id: 1,
-                        name: '',
-                        description: '',
+                        name: 'Burger King 1',
+                        description: 'Отличное место, чтио бы быстро покушать',
+                        address: 'Сурганова 5',
                         coordinates: [27.554179, 53.906461]
                     },
                     {
                         id: 2,
-                        name: '',
-                        description: '',
+                        name: 'Burger King 2',
+                        description: 'Здесь лучшие бургеры',
+                        address: 'Маяковского 15/2',
                         coordinates: [27.496666, 53.912810]
                     },
                     {
                         id: 3,
-                        name: '',
-                        description: '',
+                        name: 'Burger King 3',
+                        description: 'Ууууу луковые колечки',
+                        address: 'Победы 34',
                         coordinates: [27.594500, 53.927107]
                     },
                     {
                         id: 4,
-                        name: '',
-                        description: '',
+                        name: 'Burger King 4',
+                        description: 'Комбо за 30 рублей на двоих',
+                        address: 'Дзержинского 5',
                         coordinates: [27.509685, 53.929633]
                     },
                     {
                         id: 5,
-                        name: '',
-                        description: '',
+                        name: 'Burger King 5',
+                        description: 'Лучшее место что бы попить колы',
+                        address: 'Брикеля 8',
                         coordinates: [27.588133, 53.933288]
                     },
                 ]
@@ -136,6 +149,9 @@
             //     let lngLat = this.marker.getLngLat();
             //     console.log(lngLat)
             // }
+            showPopUp(){
+
+            }
         },
         watch: {}
         // fetchedGeoData(){
@@ -149,10 +165,30 @@
 
 <style scoped>
     .basemap {
-        width: 100vw;
+        width: 80%;
         height: 100vh;
     }
-
+    .objectWrapper{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border: 2px solid royalblue;
+        border-radius: 8px;
+        margin: 10px;
+        padding: 8px;
+    }
+    .mapAddButton{
+        border-radius: 5px;
+        padding: 5px;
+    }
+    .mapWrapper{
+        display: flex;
+    }
+    .mapNavigation{
+        width: 20%;
+        background: lightgray;
+    }
     .coordinates {
         background: rgba(0, 0, 0, 0.5);
         color: #fff;
