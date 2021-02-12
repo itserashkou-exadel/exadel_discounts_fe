0c5a7de09f13d18ecb2c6e6f341626215d3e0143
stigmaby@gmail.com <oFFline13>
2021-02-12 14:47:27 +0300

    Cp-003-Map-with-VueMapbox-v1: Added left side navbar from vuetify (navigation drawer)

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -1,5 +1,8 @@
 <template>
     <div id="map">
+        <nav>
+            <LeftSideBar/>
+        </nav>
         <MglMap
                 :accessToken="accessToken"
                 :mapStyle="mapStyle"
@@ -13,16 +16,19 @@
 <script>
     import Mapbox from "mapbox-gl";
     import { MglMap } from "vue-mapbox";
+    import LeftSideBar from "@/components/Map/LeftSideBar/LeftSideBar";
 
     export default {
         components: {
+            LeftSideBar,
             MglMap
         },
         data() {
             return {
                 accessToken: 'pk.eyJ1Ijoic3RpZ21hYnkiLCJhIjoiY2traWJpcGc5MHduNjJwcXRnYXlyM2p2ayJ9.oQtdhez6948Aq30pQWBGiA', // your access token. Needed if you using Mapbox maps
                 mapStyle: 'mapbox://styles/mapbox/streets-v11', // your map style
-                coordinates: [27.544592, 53.898477]
+                coordinates: [27.544592, 53.898477],
+
             };
         },
 
@@ -36,7 +42,7 @@
                 this.map = event.map;
                 // or just to store if you want have access from other components
                 //this.$store.map = event.map;
-            }
+            },
         }
     };
 </script>
@@ -45,5 +51,6 @@
     #map{
         width: 100vw;
         height: 100vh;
+        display: flex;
     }
 </style>
