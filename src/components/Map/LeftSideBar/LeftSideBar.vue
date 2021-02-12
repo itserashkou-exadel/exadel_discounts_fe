074f5852493c589d86612c14a84e7ef24413045b
stigmaby@gmail.com <oFFline13>
2021-02-12 15:51:00 +0300

    Cp-003-Map-with-VueMapbox-v1: Added clear (navigation drawer)

--- src/components/Map/LeftSideBar/LeftSideBar.vue
+++ src/components/Map/LeftSideBar/LeftSideBar.vue
@@ -22,26 +22,28 @@
 
             <v-divider></v-divider>
 
-            <v-list dense>
-                <v-list-item
-                        v-for="item in items"
-                        :key="item.title"
-                        link
-                >
-                    <v-list-item-icon>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-item-icon>
+<!--            <v-list dense>-->
+<!--                <v-list-item-->
+<!--                        v-for="item in fakeDataPoints2"-->
+<!--                        :key="item.id"-->
+<!--                        link-->
+<!--                >-->
 
-                    <v-list-item-content>
-                        <v-list-item-title>{{ item.title }}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
+<!--                    <v-list-item-icon>-->
+<!--                        <v-icon>{{ item.name }}</v-icon>-->
+<!--                    </v-list-item-icon>-->
+
+<!--                    <v-list-item-content>-->
+<!--                        <v-list-item-title>{{ item.description }}</v-list-item-title>-->
+<!--                    </v-list-item-content>-->
+<!--                </v-list-item>-->
+<!--            </v-list>-->
         </v-navigation-drawer>
     </v-card>
 </template>
 
 <script>
+
     export default {
         name: "LeftSideBar",
         data(){
@@ -52,7 +54,44 @@
                     { title: 'My Account', icon: 'mdi-account' },
                     { title: 'Users', icon: 'mdi-account-group-outline' },
                 ],
-                mini: true,
+                mini: false,
+                fakeDataPoints2:  [
+                    {
+                        id: '1',
+                        name: 'Burger King 1',
+                        description: 'Отличное место, чтио бы быстро покушать',
+                        address: 'Сурганова 5',
+                        coordinates: [27.554179, 53.906461]
+                    },
+                    {
+                        id: 2,
+                        name: 'Burger King 2',
+                        description: 'Здесь лучшие бургеры',
+                        address: 'Маяковского 15/2',
+                        coordinates: [27.496666, 53.912810]
+                    },
+                    {
+                        id: 3,
+                        name: 'Burger King 3',
+                        description: 'Ууууу луковые колечки',
+                        address: 'Победы 34',
+                        coordinates: [27.594500, 53.927107]
+                    },
+                    {
+                        id: 4,
+                        name: 'Burger King 4',
+                        description: 'Комбо за 30 рублей на двоих',
+                        address: 'Дзержинского 5',
+                        coordinates: [27.509685, 53.929633]
+                    },
+                    {
+                        id: 5,
+                        name: 'Burger King 5',
+                        description: 'Лучшее место что бы попить колы',
+                        address: 'Брикеля 8',
+                        coordinates: [27.588133, 53.933288]
+                    },
+                ]
             }
         }
     }
