d03866f0bcf02c6166aabfbc359eccf16249c713
Natalie <natalie.smirnowa@gmail.com>
2021-02-16 15:31:06 +0300

    cp-10: Subscriptions component was created

---
+++ src/components/Subscriptions.vue
@@ -0,0 +1,16 @@
+<template>
+<DataTable/>
+</template>
+
+<script>
+
+import DataTable from "@/components/DataTable";
+export default {
+name: "Subscriptions",
+  components: {DataTable}
+}
+</script>
+
+<style scoped>
+
+</style>
