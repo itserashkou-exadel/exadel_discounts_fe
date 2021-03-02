ef756fa04a446b94d44cf3dd750f9936c5fa6dcc
stigmaby@gmail.com <oFFline13>
2021-03-02 14:30:34 +0300

    CP-007: Commit dotenv tests

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,19 +4,22 @@ export default class AuthService {
     private userManager: UserManager;
 
     constructor() {
-        const STS_DOMAIN: string = 'https://localhost:8001';
+        const STS_DOMAIN: string = `${process.env.VUE_APP_URL_STS}`;
+        //const STS_DOMAIN: string = 'https://localhost:8001';
 
         const settings: any = {
             userStore: new WebStorageStateStore({ store: window.localStorage }),
             authority: STS_DOMAIN,
             client_id: 'crazypriceclient',
-            redirect_uri: 'https://localhost:44357/callback.html',
-            //redirect_uri: `https://${process.env.VUE_APP_URL}:44357/callback.html`,
+            //redirect_uri: 'https://localhost:44357/callback.html',
+            redirect_uri: `${process.env.VUE_APP_URL}/callback.html`,
             automaticSilentRenew: true,
-            silent_redirect_uri: 'https://localhost:44357/silent-renew.html',
+            silent_redirect_uri: `${process.env.VUE_APP_URL}/silent-renew.html`,
+            //silent_redirect_uri: `${process.env.VUE_APP_URL}/silent-renew.html`,
             response_type: 'code',
             scope: 'openid profile role crazy_price_api1',
-            post_logout_redirect_uri: 'https://localhost:44357',
+            post_logout_redirect_uri: `${process.env.VUE_APP_URL}`,
+            //post_logout_redirect_uri: 'https://localhost:44357',
             filterProtocolClaims: true,
         };
 
--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,25 @@
-
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const fs = require('fs')
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new CopyWebpackPlugin([
-          { from: 'node_modules/oidc-client/dist/oidc-client.min.js', to: 'js' }
-      ])
+    configureWebpack: {
+        plugins: [
+            new CopyWebpackPlugin([
+                {from: 'node_modules/oidc-client/dist/oidc-client.min.js', to: 'js'}
+            ])
+        ]
+    },
+
+    devServer: {
+        //host: `${process.env.BASE_URL}`,
+        host: 'localhost',
+        disableHostCheck: true,
+        https: {},
+        public: 'https://localhost:44357',
+        //public: `https://${process.env.BASE_URL}`,
+    },
+
+    transpileDependencies: [
+        'vuetify'
     ]
-  },
-
-  devServer: {
-      //host: `${process.env.BASE_URL}`,
-      host: 'localhost',
-      disableHostCheck: true,
-      https: {
-
-      },
-      public: 'https://localhost:44357',
-      //public: `https://${process.env.BASE_URL}`,
-      //publicPath: `https://${process.env.BASE_URL}`
-  },
-
-  transpileDependencies: [
-    'vuetify'
-  ]
 }
