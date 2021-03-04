32f1203b62d5f7320de3ea764112765478e1bb7e
stigmaby@gmail.com <oFFline13>
2021-03-04 18:54:47 +0300

    CP-006: Changes in map view

--- src/components/Map/DetailPageMap.vue
+++ src/components/Map/DetailPageMap.vue
@@ -34,7 +34,6 @@
         },
         mounted() {
             this.getProps()
-            console.log('dataInfo: ', this.dataInfo)
         },
         methods: {
             async onMapLoaded(event) {
